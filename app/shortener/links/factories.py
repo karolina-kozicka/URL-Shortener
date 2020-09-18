ffaafcbc5ebd741461609ded21f3ec22bddff2c5
Karolina Kozicka <karolina.anna.kozicka@gmail.com>
2020-09-19 01:28:04 +0200

    Add tests for OpenLinkView

--- app/shortener/links/factories.py
+++ app/shortener/links/factories.py
@@ -1,19 +1,20 @@
 import datetime
 import factory
 from django.utils import timezone
- 
+
 
 from . import models
 from shortener.users.factories import UserFactory
 
+
 class LinkFactory(factory.django.DjangoModelFactory):
-    
     class Meta:
         model = models.Link
 
-    url = factory.Sequence(lambda n: f"original-url{n}")
+    url = factory.Sequence(lambda n: f"http://original-url{n}.com")
     hash = factory.Sequence(lambda n: f"hash{n}")
     user = factory.SubFactory(UserFactory)
     valid_date = timezone.now() + datetime.timedelta(days=15)
     views = 0
-        
+    password = ""
+
--- app/shortener/links/tests/tests_views.py
+++ app/shortener/links/tests/tests_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from .. import views
+from .. import views, forms
 from ..factories import LinkFactory
 from shortener.users.factories import UserFactory
 from shortener.users.views import LoginView
@@ -77,3 +77,78 @@ class LinksDetailViewTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["link"], link)
+
+
+class OpenLinkViewTests(TestCase):
+    def test_uses_open_link_view(self):
+        link = LinkFactory.create()
+        url = reverse("open", args=(link.hash,))
+        response = self.client.get(url)
+        self.assertEqual(
+            response.resolver_match.func.__name__, views.OpenLinkView.as_view().__name__
+        )
+
+    def test_redirects_to_link_url_if_link_is_without_password(self):
+        link = LinkFactory.create()
+        url = reverse("open", args=(link.hash,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, link.url)
+
+    def test_renders_password_template_if_link_is_with_password(self):
+        link = LinkFactory.create(password="password")
+        url = reverse("open", args=(link.hash,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name[0], "links/password.html")
+
+    def test_puts_password_form_to_context(self):
+        link = LinkFactory.create(password="password")
+        url = reverse("open", args=(link.hash,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context["form"], forms.PasswordForm)
+
+    def test_redirects_to_link_if_password_is_correct(self):
+        link_password = "password"
+        link = LinkFactory.create(password=link_password)
+        url = reverse("open", args=(link.hash,))
+        data = {"password": link_password}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, link.url)
+
+    def test_renders_password_template_if_password_is_incorrect(self):
+        link_password = "password"
+        link = LinkFactory.create(password=link_password)
+        url = reverse("open", args=(link.hash,))
+        data = {"password": "incorrect_password"}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name[0], "links/password.html")
+
+    def test_increments_views_properly(self):
+        link_password = "password"
+        start_views = 0
+        link_with_password = LinkFactory.create(
+            password=link_password, views=start_views
+        )
+        link_without_password = LinkFactory.create(password="", views=start_views)
+
+        url = reverse("open", args=(link_with_password.hash,))
+
+        data = {"password": "incorrect_password"}
+        self.client.post(url, data)
+        link_with_password.refresh_from_db()
+        self.assertEqual(link_with_password.views, start_views)
+
+        data = {"password": link_password}
+        self.client.post(url, data)
+        link_with_password.refresh_from_db()
+        self.assertEqual(link_with_password.views, start_views + 1)
+
+        url = reverse("open", args=(link_without_password.hash,))
+
+        self.client.get(url)
+        link_without_password.refresh_from_db()
+        self.assertEqual(link_without_password.views, start_views + 1)
